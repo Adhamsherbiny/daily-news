2e3253c88d1b8801f15dea2d5a04ee2eefa93803
adhamsherbiny <adhamsherbiny.business@hotmail.com>
2024-10-27 21:35:34 +0300

    new update 0.0.7

--- app/News.tsx
+++ app/News.tsx
@@ -41,14 +41,23 @@ import { useEffect, useState } from "react";
 async function getData() {
   try {
     const resultOne = await axios.get(
-      "http://newsapi.org/v2/everything?q=apple&from=2024-10-15&to=2024-10-15&sortBy=popularity&apiKey=203b419c3c484293835f919f943ff0cc"
+      "http://newsapi.org/v2/everything?q=apple&from=2024-10-15&to=2024-10-15&sortBy=popularity&apiKey=203b419c3c484293835f919f943ff0cc",
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
     );
 
     const resultTwo = await axios.get(
-      "http://api.nytimes.com/svc/topstories/v2/arts.json?api-key=hx5y3viP9blbRE9knf5Wy4BgYwNDda18"
+      "http://api.nytimes.com/svc/topstories/v2/arts.json?api-key=hx5y3viP9blbRE9knf5Wy4BgYwNDda18",
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
     );
 
-    // Safely merge data, ensuring both arrays exist
     const allData = [
       ...(resultOne.data.articles || []),
       ...(resultTwo.data.results || []),
